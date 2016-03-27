486aef2b11d82488de4420734d792c8c50d5cb0e
Keyu Lai <kl2844@columbia.edu>
2016-03-27 11:56:53 -0400

    add db part for main page

--- website/application.py
+++ website/application.py
@@ -1,12 +1,55 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 import requests
 import json
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
 
 application = Flask(__name__)
 
+DATABASEURI = "postgresql://kl2844:PKKNNH@w4111db.eastus.cloudapp.azure.com/kl2844"
+engine = create_engine(DATABASEURI)
+
+@application.before_request
+def before_request():
+	"""
+	This function is run at the beginning of every web request 
+	(every time you enter an address in the web browser).
+	We use it to setup a database connection that can be used throughout the request
+
+	The variable g is globally accessible
+	"""
+	try:
+		g.conn = engine.connect()
+	except:
+		print "uh oh, problem connecting to database"
+		import traceback; traceback.print_exc()
+		g.conn = None
+
+@application.teardown_request
+def teardown_request(exception):
+	"""
+	At the end of the web request, this makes sure to close the database connection.
+	If you don't the database could run out of memory!
+	"""
+	try:
+		g.conn.close()
+	except Exception as e:
+		pass
+
 @application.route('/')
 def main_page():
-	return render_template('index.html') 
+	context = dict()
+	cursor = g.conn.execute("SELECT name, description FROM Event_Locates LIMIT 6")
+	context['events'] = [event for event in cursor]
+	cursor.close()
+	cursor = g.conn.execute("SELECT name, location FROM Venues LIMIT 6")
+	context['venues'] = [venue for venue in cursor]
+	cursor.close()
+	cursor = g.conn.execute("SELECT name, type, image FROM Performers LIMIT 6")
+	context['performers'] = [performer for performer in cursor]
+	cursor.close()
+	print context['performers']
+	return render_template('index.html', **context) 
 
 @application.route('/<list_name>')
 def events_list(list_name=None):
@@ -27,5 +70,4 @@ def surround():
 	return r.text
 
 if __name__ == '__main__':
-	application.debug = True
-	application.run()
+	application.run(debug=True)
