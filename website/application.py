86176e251629017d2debab435e6426430f7ccbb3
Keyu Lai <kl2844@columbia.edu>
2016-03-28 17:17:58 -0400

    add favo_tickets

--- website/application.py
+++ website/application.py
@@ -144,6 +144,9 @@ def users(id=None):
 	context['reviews'] = get_sql("SELECT V.name, V.vid, R.content, R.rating \
 								  FROM Reviews AS R, Venues AS V \
 								  WHERE R.vid=V.vid AND R.uid=%s", id)
+	context['tickets'] = get_sql("SELECT T.tid, T.listing_count, T.price, T.happen_date, T.url  \
+								  FROM Has_Tickets AS T, Favors AS F \
+								  WHERE T.tid=F.tid AND F.uid=%s", id)
 	return render_template('user.html', **context) 
 
 @application.route('/search')
